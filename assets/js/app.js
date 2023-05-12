60b6e447f9e11cf3dc4b5c114ef83f5bc2769358
Siria <sirifranco00@gmail.com>
2023-05-12 11:31:45 -0700

    review of an exercise with some mistakes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,25 @@
+/* El código importa información API por medio de fetch de un repositorio externo para mostrarlo en el documento */
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+ const $n = document.querySelector('.name'); //se renombró const por un nombre que permita relacionarla con su contenido
+const $b = document.querySelector('.blog'); //se renombró const por un nombre que permita relacionarla con su contenido
+const $l = document.querySelector('.location'); //se renombró const por un nombre que permita relacionarla con su contenido
 
-function displayUser(username) {
+ async function displayUser(username) { //se colocó async para declarar la función correspondiente con await
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json(); // se añade esta línea para obtener datos en json
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; //se cambian las comillas por acento grave para funcionar 
+  $b.textContent = `${data.blog}`; //se cambian las comillas por acento grave para funcionar 
+  $l.textContent = `${data.location}`; //se cambian las comillas por acento grave para funcionar 
 }
 
-function handleError(err) {
+function handleError(err, $n) { // se anade el $n para llamarlo
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`; //se añade signo $ para que se llame a n y el;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError); 
